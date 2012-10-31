55f84c8ab103ef1c6d19302646703a6397f9279b
Hanno Schlichting <hanno@hannosch.eu>
2012-10-31 14:10:05 +0000

    LP #1071067: Use a stronger random number generator and a constant time comparison function.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from os.path import join
 from setuptools import setup, find_packages, Extension
 
 setup(name='AccessControl',
-      version='3.0.6dev',
+      version='3.0.6',
       url='http://pypi.python.org/pypi/AccessControl',
       license='ZPL 2.1',
       description="Security framework for Zope2.",
