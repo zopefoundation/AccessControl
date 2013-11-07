0cbc1054c2c923fde38d442189ec012e2cc3c8a7
Tres Seaver <tseaver@palladion.com>
2013-11-07 14:11:17 -0500

    Move MUT import out of module scope.

--- src/AccessControl/tests/testRole.py
+++ src/AccessControl/tests/testRole.py
@@ -1,35 +1,45 @@
 import unittest
 
-from AccessControl.rolemanager import RoleManager
-from Acquisition import Implicit, Explicit
+
+def _makeRootAndUser():
+    from AccessControl.rolemanager import RoleManager
+    from Acquisition import Implicit, Explicit
 
 
-class DummyContext(Implicit, RoleManager):
-    __roles__ = ('Manager',)
+    class DummyContext(Implicit, RoleManager):
+        __roles__ = ('Manager',)
 
 
-class DummyUser(Explicit):
-    def getRoles(self):
-        return ('Manager',)
+    class DummyUser(Explicit):
+        def getRoles(self):
+            return ('Manager',)
 
-    def getRolesInContext(self, context):
-        return ('Manager',)
+        def getRolesInContext(self, context):
+            return ('Manager',)
 
-    def has_permission(self, permission, context):
-        return True
-
-
-class DummyAclUsers(Explicit):
-    def getUser(self, user_id):
-        user = DummyUser()
-        return user.__of__(self)
-
-    def absolute_url(self, relative=0):
-        return 'acl_users'
+        def has_permission(self, permission, context):
+            return True
 
 
-class DummyRoot(Explicit):
-    acl_users = DummyAclUsers()
+    class DummyAclUsers(Explicit):
+        def getUser(self, user_id):
+            user = DummyUser()
+            return user.__of__(self)
+
+        def absolute_url(self, relative=0):
+            return 'acl_users'
+
+
+    class DummyRoot(Explicit):
+        acl_users = DummyAclUsers()
+
+    root = DummyRoot()
+    root.acl_users = DummyAclUsers()
+    root.context1 = DummyContext()
+    root.context2 = DummyContext()
+    user = DummyUser().__of__(root.acl_users)
+
+    return root, user
 
 
 class TestRoleManager(unittest.TestCase):
@@ -49,13 +59,8 @@ class TestRoleManager(unittest.TestCase):
         from AccessControl.ImplPython import verifyAcquisitionContext
         from AccessControl.SecurityManagement import getSecurityManager
         from AccessControl.SecurityManagement import newSecurityManager
-
-        root = DummyRoot()
-        root.acl_users = DummyAclUsers()
-        user = DummyUser().__of__(root.acl_users)
+        root, user = _makeRootAndUser()
         newSecurityManager(None, user)
-        root.context1 = DummyContext()
-        root.context2 = DummyContext()
         root.context1.manage_getUserRolesAndPermissions('dummy_user')
         user = getSecurityManager().getUser()
-        self.assertIsNotNone(verifyAcquisitionContext(user, root.context2, ()))
+        self.assertTrue(verifyAcquisitionContext(user, root.context2, ()))
