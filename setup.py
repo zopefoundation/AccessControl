3a226ccea74d767278733c2bd7fca3251cccdf24
Hanno Schlichting <hanno@hannosch.eu>
2017-02-15 12:24:51 +0100

    Indent setup.py, test against Zope 2.13 KGS.

--- setup.py
+++ setup.py
@@ -15,43 +15,45 @@
 from os.path import join
 from setuptools import setup, find_packages, Extension
 
+__version__ = '2.13.15.dev0'
+
 setup(name='AccessControl',
-      version = '2.13.15dev',
+      version=__version__,
       url='http://pypi.python.org/pypi/AccessControl',
       license='ZPL 2.1',
       description="Security framework for Zope2.",
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
-      long_description=open('README.txt').read() + '\n' +
-                       open('CHANGES.txt').read(),
-
+      long_description=(open('README.txt').read() + '\n' +
+                        open('CHANGES.txt').read()),
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      ext_modules=[Extension(
-            name='AccessControl.cAccessControl',
-            include_dirs=['include', 'src'],
-            sources=[join('src', 'AccessControl', 'cAccessControl.c')],
-            depends=[join('include', 'ExtensionClass', 'ExtensionClass.h'),
-                     join('include', 'Acquisition', 'Acquisition.h')]),
+      ext_modules=[
+          Extension(
+              name='AccessControl.cAccessControl',
+              include_dirs=['include', 'src'],
+              sources=[join('src', 'AccessControl', 'cAccessControl.c')],
+              depends=[join('include', 'ExtensionClass', 'ExtensionClass.h'),
+                       join('include', 'Acquisition', 'Acquisition.h')]),
       ],
       install_requires=[
-        'Acquisition',
-        'DateTime', # optional dependency of RestrictedPython
-        'ExtensionClass',
-        'Persistence',
-        'Record',
-        'RestrictedPython',
-        'transaction',
-        'zExceptions',
-        'ZODB3',
-        'zope.component',
-        'zope.configuration',
-        'zope.deferredimport',
-        'zope.interface',
-        'zope.publisher',
-        'zope.schema',
-        'zope.security',
-        'zope.testing',
+          'Acquisition',
+          'DateTime',  # optional dependency of RestrictedPython
+          'ExtensionClass',
+          'Persistence',
+          'Record',
+          'RestrictedPython',
+          'transaction',
+          'zExceptions',
+          'ZODB3',
+          'zope.component',
+          'zope.configuration',
+          'zope.deferredimport',
+          'zope.interface',
+          'zope.publisher',
+          'zope.schema',
+          'zope.security',
+          'zope.testing',
       ],
       include_package_data=True,
       zip_safe=False,
