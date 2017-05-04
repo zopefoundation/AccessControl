c082ad570c99bbe226509b6c835d18f075756272
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-05-04 18:33:35 +0200

    isort

--- src/AccessControl/metaconfigure.py
+++ src/AccessControl/metaconfigure.py
@@ -12,12 +12,11 @@
 #
 ##############################################################################
 
-import warnings
-
-from zope.security import metaconfigure
-
 from AccessControl.class_init import InitializeClass
 from AccessControl.security import protectName
+from zope.security import metaconfigure
+
+import warnings
 
 
 class ClassDirective(metaconfigure.ClassDirective):
