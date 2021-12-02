7448b5b04210af8bf51515e187b5c2ef4fd6d6ff
dieter <dieter@handshake.de>
2021-12-02 10:51:10 +0100

    add `gc.collect` calls to let the `testIdentityProxy` test succeed

--- src/AccessControl/tests/testZopeSecurityPolicy.py
+++ src/AccessControl/tests/testZopeSecurityPolicy.py
@@ -11,6 +11,7 @@
 #
 ##############################################################################
 
+import gc
 import sys
 import os
 import unittest
@@ -244,11 +245,14 @@ class ZopeSecurityPolicyTestBase(unittest.TestCase):
         eo = ImplictAcqObject()
         eo.getOwner = lambda: None
         self.context.stack.append(eo)
+        gc.collect()
         rc = sys.getrefcount(eo)
         self.testUserAccess()
+        gc.collect()
         self.assertEqual(rc, sys.getrefcount(eo))
         eo._proxy_roles = ()
         self.testUserAccess()
+        gc.collect()
         self.assertEqual(rc, sys.getrefcount(eo))
 
     def testAccessToUnprotectedSubobjects(self):
